34e3f3efe5b586d2f4dbbcffd779f4323d94b8df
CristianMauricioPicoRivero <cristian.pico@outlook.com>
2021-07-12 21:11:34 -0500

    Operadores_asignacion_12_07_2021

---
+++ main.js
@@ -0,0 +1,62 @@
+
+
+let x = "5";
+let y = "3";
+
+
+// x %= y;
+// console.log(x);
+// x /= y;
+// console.log(Math.trunc(x));
+//  5  /3
+//  2 
+//     
+
+// Operador de Exponenciación
+// x **= y;
+// x = x ** y;
+// console.log(x);
+
+
+
+// Operador de Resto
+// x %= y;
+// x = x % y;
+// console.log(x);
+
+
+
+// Operador de División
+// x /= y;
+// x = x/y;
+// console.log(x);
+
+
+
+// Operador de Multiplicacion
+// x *= y;
+// x = x*y;
+// console.log(x);
+
+
+
+// Operador de Subtracion
+// x -= y;
+// x = x - y;
+// console.log(x, typeof(x));
+
+
+
+// Operador de Adicion
+// console.log(`Valor x inicializada ${x}`);
+// console.log(`Valor y inicializada  ${y}`);
+// x += y;
+// x = x + y;
+// console.log(`Valor x operador de adicion ${x}`);
+// console.log(`Valor y operador de adicion ${y}`);
+
+
+// Operador de asignación
+
+// x = y;
+// console.log(x, typeof(x));
